96f3f932f9137d639784b9fcbc56e503af210fb5
Matthew Malkin <matthew@codeconcepts.co.uk>
2017-04-30 16:29:59 +0100

    fix csproj, add nosql snapshot repo without implementation as placeholder for directory

---
+++ Policy.Plugin.Isa.Policy.DataAccess/NoSql/SinglePolicySnapshotNoSqlStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Policy.Application.Interfaces;
+using Policy.Application.Interfaces.Repositories;
+using Policy.Plugin.Isa.Policy.Interfaces.Domain;
+using Policy.Plugin.Isa.Policy.Views.PolicyView;
+
+namespace Policy.Plugin.Isa.Policy.DataAccess.NoSql
+{
+    public class SinglePolicySnapshotNoSqlStore : ISnapshotStore<PolicyView, IPolicyContext>
+    {
+        public ISnapshot<PolicyView, IPolicyContext> Get(Guid contextId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(PolicyView view, IEvent<IPolicyContext> @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
