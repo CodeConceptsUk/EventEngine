c37f195a748c3152ad261ed59dab2eaf02c1839b
Matthew Malkin <matthew@codeconcepts.co.uk>
2017-05-05 21:12:25 +0100

    added Unit Test project for Policy.Application

---
+++ Policy.Application.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Policy.Application.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Policy.Application.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46c7a280-d8b4-4d56-ad69-ab780b519105")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Policy.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Policy.Application.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Inconclusive("Not Yet Written");
+        }
+    }
+}
