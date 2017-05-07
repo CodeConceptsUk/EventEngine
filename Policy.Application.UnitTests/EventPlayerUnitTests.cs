dd94253b491d6f0ceb40c6bc3d87ae91e4b1c22c
Matthew Malkin <matthew@codeconcepts.co.uk>
2017-05-07 23:42:28 +0100

    setup for test of eventPlayer

---
+++ Policy.Application.UnitTests/EventPlayerUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Unity;
+using NSubstitute;
+using NUnit.Framework;
+using Policy.Application.Interfaces;
+using Policy.Application.Interfaces.Factories;
+
+namespace Policy.Application.UnitTests
+{
+    [TestFixture]
+    public class EventPlayerUnitTests
+    {
+        private IEventPlayer<IEvent> _target;
+        private IUnityContainer _container;
+        private ILogFactory _logFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logFactory = Substitute.For<ILogFactory>();
+            _container = Substitute.For<IUnityContainer>();
+            _target = new EventPlayer<IEvent>(_container, _logFactory);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Inconclusive("Not Yet Written");
+        }
+    }
+}
