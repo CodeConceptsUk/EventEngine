f441130728ddf8cb8059e9b08da28aa2d8d28865
Matthew Malkin <matthew@codeconcepts.co.uk>
2017-05-12 00:17:34 +0100

    refactored commands for IsaPolicy into contracts

---
+++ Application/EventEngine.IsaPolicy.Contracts/Commands/AddPremiumAsReceivedCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Policy.Plugin.Isa.Policy.Operations.BaseTypes;
+using Policy.Plugin.Isa.Policy.Operations.PropertyBags;
+
+namespace Policy.Plugin.Isa.Policy.Operations.Commands
+{
+    [DataContract(Namespace = "http://codeconcepts.co.uk/commands")]
+    public class AddPremiumAsReceivedCommand : IsaPolicyCommand
+    {
+        public AddPremiumAsReceivedCommand(string policyNumber, string premiumId, DateTime premiumDateTime, IEnumerable<FundPremiumDetail> fundPremiumDetail)
+        {
+            PolicyNumber = policyNumber;
+            PremiumId = premiumId;
+            PremiumDateTime = premiumDateTime;
+            FundPremiumDetail = fundPremiumDetail;
+        }
+
+        [DataMember]
+        public string PolicyNumber { get; set; }
+
+        [DataMember]
+        public string PremiumId { get; set; }
+
+        [DataMember]
+        public DateTime PremiumDateTime { get; set; }
+
+        [DataMember]
+        public IEnumerable<FundPremiumDetail> FundPremiumDetail { get; set; }
+    }
+}
