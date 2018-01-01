aa9c6fc745d13f27af89ced2355488be15f8ecb1
Andrew Duff <andrew@codeconcepts.co.uk>
2018-01-01 11:38:53 +0000

    Added DateTimeProvider

---
+++ EventEngine.Application/Providers/DateTimeProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EventEngine.Application.Interfaces.Providers;
+
+namespace EventEngine.Application.Providers
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime GetLocalTime()
+        {
+            return DateTime.Now.ToLocalTime();
+        }
+
+        public DateTime GetUtcTime()
+        {
+            return DateTime.Now.ToUniversalTime();
+        }
+    }
+}
---
+++ EventEngine.UnitTests/Providers/DateTimeProviderUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EventEngine.Application.Interfaces.Providers;
+using EventEngine.Application.Providers;
+using NUnit.Framework;
+
+namespace EventEngine.UnitTests.Providers
+{
+    [TestFixture]
+    public class DateTimeProviderUnitTests
+    {
+        private IDateTimeProvider _target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _target = new DateTimeProvider();
+        }
+
+        [Test]
+        public void WhenIGetTheLocalDateTimeItIsReturned()
+        {
+            var expectedDateTime = DateTime.Now.ToLocalTime();
+
+            var result = _target.GetLocalTime();
+
+            Assert.That(result, Is.EqualTo(expectedDateTime).Within(TimeSpan.FromSeconds(1)));
+        }
+
+        [Test]
+        public void WhenIGetTheUtcDateTimeItIsReturned()
+        {
+            var expectedDateTime = DateTime.Now.ToUniversalTime();
+
+            var result = _target.GetUtcTime();
+
+            Assert.That(result, Is.EqualTo(expectedDateTime).Within(TimeSpan.FromSeconds(1)));
+        }
+    }
+}
