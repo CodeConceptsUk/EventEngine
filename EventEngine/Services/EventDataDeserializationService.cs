fdcc40e0e1a68e704785812d29994cdf9ce08b8c
Matthew Malkin <matt@zercat.net>
2018-02-05 14:08:58 +0000

    serialization pt1

--- EventEngine/Services/EventDataDeserializationService.cs
+++ EventEngine/Services/EventDataDeserializationService.cs
@@ -4,11 +4,27 @@ using Newtonsoft.Json;
 
 namespace EventEngine.Services
 {
+    internal static class JsonHelper
+    {
+        public static JsonSerializer Serializer = JsonSerializer.Create(Settings);
+        public static JsonSerializerSettings Settings = new JsonSerializerSettings {
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            ObjectCreationHandling = ObjectCreationHandling.Auto,
+            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+            TypeNameHandling = TypeNameHandling.All,
+            DateParseHandling = DateParseHandling.None,
+            DateTimeZoneHandling = DateTimeZoneHandling.RoundtripKind,
+            Formatting = Formatting.Indented
+        };
+    }
+
     public class EventDataDeserializationService : IEventDataDeserializationService
     {
         public object Deserialize(Type eventDataType, string eventData)
         {
-            return JsonConvert.DeserializeObject(eventData, eventDataType);
+            return JsonHelper.Serializer.DeserializeObject(eventData, eventDataType);
         }
     }
 }
