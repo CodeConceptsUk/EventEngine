dbd3113d51653f476664ccafcf069b42dcb59b1b
Matthew Malkin <m@m3z.net>
2018-03-02 12:18:40 +0000

    missed test

--- EventEngine.UnitTests/PropertyBags/EventUnitTests.cs
+++ EventEngine.UnitTests/PropertyBags/EventUnitTests.cs
@@ -39,6 +39,29 @@ namespace EventEngine.UnitTests.PropertyBags
             _eventDataSerializationService.Serialize(eventData).Returns(expectedEventData);
             _eventTypeService.Get<IEventData>().Returns(expectedEventType);
 
+            var result = _target.Create(expectedContextId, eventData, expectedEffectiveDateTime);
+
+            Assert.Equal(expectedContextId, result.ContextId);
+            Assert.Equal(expectedEventDateTime, result.CreatedDateTime);
+            Assert.Equal(expectedEffectiveDateTime, result.EffectiveDateTime);
+            Assert.Same(expectedEventType, result.EventType);
+            Assert.Equal(expectedEventData, result.EventData);
+        }
+        
+        [Fact]
+        public void WhenICreateAnEventWithDefaultEffectiveDateItIsCreated()
+        {
+            var expectedContextId = Guid.NewGuid();
+            var expectedEventData = Guid.NewGuid().ToString();
+            var expectedEventType = Substitute.For<IEventType>();
+            var expectedEventDateTime = DateTime.Now;
+            var expectedEffectiveDateTime = expectedEventDateTime;
+            var eventData = Substitute.For<IEventData>();
+
+            _dateTimeProvider.GetUtc().Returns(expectedEventDateTime);
+            _eventDataSerializationService.Serialize(eventData).Returns(expectedEventData);
+            _eventTypeService.Get<IEventData>().Returns(expectedEventType);
+
             var result = _target.Create(expectedContextId, eventData);
 
             Assert.Equal(expectedContextId, result.ContextId);
