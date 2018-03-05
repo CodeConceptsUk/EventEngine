61dcf7c761cbb405d6226e5df42137b809b57aea
Matthew Malkin <m@m3z.net>
2018-03-05 21:07:36 +0000

    remove useless stuff

--- EventEngine.ExampleApplication/ContainerFactory.cs
+++ EventEngine.ExampleApplication/ContainerFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using EventEngine.Interfaces.Commands;
-using EventEngine.Interfaces.Events;
-using EventEngine.Interfaces.Factories;
-using EventEngine.Interfaces.Repositories;
+﻿using EventEngine.Interfaces.Repositories;
 using StructureMap;
 
 namespace EventEngine.ExampleApplication
@@ -29,24 +23,6 @@ namespace EventEngine.ExampleApplication
                 
             );
             return container;
-        }
-        
-        public static IEnumerable<IEventEvaluator> GetAllEventEvaluators(IContainer container)
-        {
-            var types = typeof(ContainerFactory).Assembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IEventEvaluator)));
-            foreach (var type in types)
-            {
-                yield return (IEventEvaluator)Activator.CreateInstance(type);
-            }
-        }
-
-        public static IEnumerable<ICommandHandler> GetAllCommandHandlers(IContainer container, IEventFactory eventFactory)
-        {
-            var types = typeof(ContainerFactory).Assembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(ICommandHandler)));
-            foreach (var type in types)
-            {
-                yield return (ICommandHandler)Activator.CreateInstance(type, eventFactory);
-            }
         }
     }
 }
